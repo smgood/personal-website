af5edc2a05113e34ff229bfdec830cb86e1af297
Sean Goodrich <sean@smgoodrich.com>
2019-08-21 01:11:07 -0700

    Clean up portfolioVideo class

--- portfolioVideo.js
+++ portfolioVideo.js
@@ -1,64 +1,65 @@
 // JavaScript Document
 class PortfolioVideo {
-  constructor(project_info, project_number, loadedCallback, errorCallback) {
+  constructor(projectInfo, projectNumber, loadedCallback, errorCallback){
     this.loadedCallback = loadedCallback;
     this.errorCallback = errorCallback;
 
     this.loadingManager = this.loadingManager.bind(this);
     this.errorManager = this.errorManager.bind(this);
 
-    this.mesh = this.createMesh(project_info, project_number);
+    this.mesh = this.createMesh(projectInfo, projectNumber);
   }
 
-  createMesh(project_info, project_number) {
-    var video = this.createVideo("textures/" + project_info.video);
-    var texture = new THREE.VideoTexture( video );
+  createMesh(projectInfo, projectNumber){
+    let video = this.createVideo("textures/" + projectInfo.video);
+    let texture = new THREE.VideoTexture(video);
     texture.minFilter = THREE.LinearFilter;
     texture.magFilter = THREE.LinearFilter;
     texture.format = THREE.RGBFormat;
 
-    var xsize = 500;
-    var ysize = 500;
+    let xsize = 500;
+    let ysize = 500;
 
-    var parameters = { color: 0xAAAAAA, map: texture, vertexColors: THREE.FaceColors },
+    let parameters = {
+      color: 0xAAAAAA,
+      map: texture,
+      vertexColors: THREE.FaceColors
+    };
 
-    geometry = new THREE.BoxGeometry( xsize, ysize, xsize/12 );
-    geometry.faces[ 10 ].color.setHex( 0x4C4C4C );
-    geometry.faces[ 11 ].color.setHex( 0x4C4C4C );
+    geometry = new THREE.BoxGeometry(xsize, ysize, xsize/12);
+    geometry.faces[10].color.setHex(0x4C4C4C);
+    geometry.faces[11].color.setHex(0x4C4C4C);
 
-    this.change_uvs( geometry, xsize, ysize );
+    this.changeUvs(geometry, xsize, ysize);
 
-    var material = new THREE.MeshLambertMaterial( parameters );
-    var mesh = new THREE.Mesh( geometry, material );
+    let material = new THREE.MeshLambertMaterial(parameters);
+    let mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0, (-640 * (projectNumber%2)) + 320, 0);
 
-    mesh.position.x = 0;
-    mesh.position.y = (-640 * (project_number%2)) + 320;
-    mesh.position.z = 0;
-
-    var phi = (Math.floor(project_number/2) * Math.PI/4);
+    let phi = (Math.floor(projectNumber/2) * Math.PI/4);
     mesh.rotation.y = (phi);
-    mesh.targetX = 800 * Math.sin( phi );
-    mesh.targetZ = 800 * Math.cos( phi );
+    mesh.targetX = 800 * Math.sin(phi);
+    mesh.targetZ = 800 * Math.cos(phi);
 
     mesh.visible = false;
     mesh.scale.x = mesh.scale.y = mesh.scale.z = 1;
 
     mesh.video = video;
-    mesh.info = project_info;
+    mesh.info = projectInfo;
 
     return mesh;
   }
 
-  createVideo(videoURL) {
-    var video = document.createElement('video');
-    var source = document.createElement('source');
+  createVideo(videoURL){
+    let video = document.createElement('video');
+    let source = document.createElement('source');
     video.appendChild(source);
 
-    var agent = navigator.userAgent;
-    var isIphone = ((agent.indexOf('iPhone') != -1) || (agent.indexOf('iPod') != -1)) ;
-    if(isIphone) {
+    let agent = navigator.userAgent;
+    let isIphone = ((agent.indexOf('iPhone') != -1) || (agent.indexOf('iPod') != -1)) ;
+    if(isIphone){
       video.autoplay=true;
-      video.onloadstart = function() {
+      video.onloadstart = function(){
         video.pause();
       };
     } else {
@@ -80,56 +81,56 @@ class PortfolioVideo {
     return video;
   };
 
-  loadingManager() {
+  loadingManager(){
     this.loadedCallback();
   }
 
-  errorManager() {
+  errorManager(){
     this.errorCallback();
   }
 
-  change_uvs(geometry, width, height) {
-    var faceVertexUvs = geometry.faceVertexUvs[ 0 ];
+  changeUvs(geometry, width, height){
+    let faceVertexUvs = geometry.faceVertexUvs[0];
 
-    for (var i = 0; i < faceVertexUvs.length; i++ ) {
+    for (let i = 0; i < faceVertexUvs.length; i++){
       //front
-      var unitx = 12/14;
-      var unity = 12/14;
-      var offsetx = 1/14;
-      var offsety = 1/14;
+      let unitx = 12/14;
+      let unity = 12/14;
+      let offsetx = 1/14;
+      let offsety = 1/14;
 
       //right
-      if (i == 0 || i == 1){
+      if (i === 0 || i === 1){
         unitx = 1/14;
         offsetx = 13/14;
       }
        //left
-      else if (i == 2 || i == 3){
+      else if (i === 2 || i === 3){
         unitx = 1/14;
         offsetx = 0;
       }
       //top
-      else if (i == 4 || i == 5){
+      else if (i === 4 || i === 5){
         unity = 1/14;
         offsety = 13/14;
       }
       //bottom
-      else if (i == 6 || i == 7){
+      else if (i === 6 || i === 7){
         unity = 1/14;
         offsety = 0;
       }
       //back
-      else if (i == 10 || i == 11){
-        var unitx = -12/14;
-        var offsetx = 13/14;
+      else if (i === 10 || i === 11){
+        unitx = -12/14;
+        offsetx = 13/14;
       }
 
-      var uvs = faceVertexUvs[ i ];
+      let uvs = faceVertexUvs[i];
 
-      for (var j = 0; j < uvs.length; j++ ){
-        var uv = uvs[ j ];
-        uv.x = ( uv.x * unitx ) + offsetx;
-        uv.y = ( uv.y * unity ) + offsety;
+      for (let j = 0; j < uvs.length; j++){
+        let uv = uvs[j];
+        uv.x = (uv.x * unitx) + offsetx;
+        uv.y = (uv.y * unity) + offsety;
       }
     }
   }
